6135f26c3c78639a5d6a9d5c8c5a5deb7e9a1b3e
nguyen tien0107 <tien100609@donga.edu.vn>
2025-05-29 22:04:34 +0700

    delete again

--- backend/contracts/interfaces/IMedicalRecord.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
-
-interface IMedicalRecord {
-    function addRecord(string memory ipfsHash) external;
-    function viewRecords(address patient) external view returns (string[] memory, uint[] memory); 
-}
